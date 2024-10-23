e58187ff19c8c3e2db3f4d6d9334069838ae53b4
MichaelGuauque <mcalejo98@gmail.com>
2024-10-23 16:13:34 -0500

    Creacion y estructuracion del proyecto

---
+++ src/main/java/com/spapatitas/SpaPatitasApplication.java
@@ -0,0 +1,13 @@
+package com.spapatitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaPatitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaPatitasApplication.class, args);
+	}
+
+}
