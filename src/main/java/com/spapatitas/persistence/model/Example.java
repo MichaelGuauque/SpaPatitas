300b068579231646add8a035cf4285b2de813cfa
MichaelGuauque <mcalejo98@gmail.com>
2024-10-27 10:49:17 -0500

    Ejemplo implementacion

---
+++ src/main/java/com/spapatitas/persistence/model/Example.java
@@ -0,0 +1,15 @@
+package com.spapatitas.persistence.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class Example {
+    private String este;
+    private String es;
+    private String unEjemplo;
+}
