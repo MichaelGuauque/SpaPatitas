827eb6b8620c52fb91f3076e80d5fa1ac52f67c3
MichaelGuauque <mcalejo98@gmail.com>
2024-11-01 11:14:25 -0500

    Implementacion repository

---
+++ src/main/java/com/spapatitas/persistence/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.spapatitas.persistence.repository;
+
+import com.spapatitas.persistence.model.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findUserEntityByUsername(String username);
+}
