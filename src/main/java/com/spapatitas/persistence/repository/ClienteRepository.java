561845e97b5a10d61340f18607e559afe7abae6c
Miguel Marin <miguel.andres.marin@correounivalle.edu.co>
2024-11-02 01:20:57 -0500

    Implementacion Repository(DAO)

---
+++ src/main/java/com/spapatitas/persistence/repository/ClienteRepository.java
@@ -0,0 +1,13 @@
+package com.spapatitas.persistence.repository;
+
+import com.spapatitas.persistence.model.Cliente;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+    Optional<Cliente> findClienteByCedula(int cedula);
+}
--- src/main/java/com/spapatitas/persistence/repository/ExampleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spapatitas.persistence.repository;
-
-import com.spapatitas.persistence.model.Cita;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExampleRepository extends CrudRepository<Cita, Long> {
-}
