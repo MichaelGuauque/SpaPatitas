22713e872026ae500a54764a352818af61361c4b
MichaelGuauque <mcalejo98@gmail.com>
2024-11-04 09:50:36 -0500

    Ejemplo implementacion

---
+++ src/main/java/com/spapatitas/service/interfaces/IExampleService.java
@@ -0,0 +1,14 @@
+package com.spapatitas.service.interfaces;
+//no lleva anotación
+public interface IExampleService {
+    /*
+    Aquí se declaran todos los métodos que requiera su clase a nivel de BD,
+    como por ejemplo:
+        -Find all
+        -Find by id
+        -save
+        -update
+        -deshabilitar
+        -habilitar
+     */
+}
