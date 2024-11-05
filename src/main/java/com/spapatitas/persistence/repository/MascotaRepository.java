115cfe86a02978811ed3df1255f5b162ad03609a
Miguel Marin <miguel.andres.marin@correounivalle.edu.co>
2024-11-04 23:18:07 -0500

    Implementacion de Servicios

--- src/main/java/com/spapatitas/persistence/repository/MascotaRepository.java
+++ src/main/java/com/spapatitas/persistence/repository/MascotaRepository.java
@@ -1,12 +1,10 @@
 package com.spapatitas.persistence.repository;
 
-import com.spapatitas.persistence.model.Cliente;
 import com.spapatitas.persistence.model.Mascota;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface MascotaRepository extends CrudRepository<Mascota, Long> {
