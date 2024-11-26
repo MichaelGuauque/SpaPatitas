c60a2535da90ed1398b2ab01c9cc82f15634acc5
Paul Figueroa <paul.figueroa@correounivalle.edu.co>
2024-11-26 13:46:53 -0500

    Implementacion Controller

---
+++ src/main/java/com/spapatitas/controller/ClientesController.java
@@ -0,0 +1,16 @@
+package com.spapatitas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/clientes")
+public class ClientesController {
+
+    @GetMapping("")
+    public String show() {
+        return "clientes/vistaClientes";
+    }
+
+}
