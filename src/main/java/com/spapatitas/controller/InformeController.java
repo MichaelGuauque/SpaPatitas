20c588c72963e3f3e6573c299a56131f2978981a
MichaelGuauque <mcalejo98@gmail.com>
2024-11-27 19:42:26 -0500

    Integracion vista informes

---
+++ src/main/java/com/spapatitas/controller/InformeController.java
@@ -0,0 +1,15 @@
+package com.spapatitas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/informes")
+public class InformeController {
+
+    @GetMapping()
+    public String show(){
+        return "informes/vistaInformes";
+    }
+}
