caa85156bc92d8eb93f7c160c0ee91197453bc3c
Santiago C <chicunque.c@gmail.com>
2024-11-28 11:19:52 -0500

    Implementación Carrito

--- src/main/java/com/spapatitas/controller/UserController.java
+++ src/main/java/com/spapatitas/controller/UserController.java
@@ -2,22 +2,20 @@ package com.spapatitas.controller;
 
 import com.spapatitas.DTO.ClienteDTO;
 import com.spapatitas.DTO.UserDTO;
-import com.spapatitas.persistence.model.Genero;
-import com.spapatitas.persistence.model.Producto;
-import com.spapatitas.persistence.model.UserEntity;
+import com.spapatitas.persistence.model.*;
 import com.spapatitas.service.implementation.ProductoService;
 import com.spapatitas.service.interfaces.IClienteService;
 import com.spapatitas.service.interfaces.IUserEntity;
+import jakarta.servlet.http.HttpSession;
 import org.apache.catalina.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,6 +26,9 @@ public class UserController {
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final IUserEntity userService;
     private final IClienteService clienteService;
+
+    List<DetalleVenta> detalles = new ArrayList<DetalleVenta>();
+    Venta venta = new Venta();
 
     @Autowired
     public UserController(IUserEntity userService, IClienteService clienteService) {
@@ -80,4 +81,106 @@ public class UserController {
         return "productos/vistaProductosUsuario";
     }
 
+    @PostMapping("/carrito")
+    public String añadirCarrito(@RequestParam (required = false) Long codigo,
+                                @RequestParam(defaultValue = "1") int cantidad,
+                                Model model) {
+        // Buscar el producto
+        Producto producto = productoService.getProductoRepository().findById(codigo)
+                .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+
+        // Verificar si el producto ya está en el carrito
+        Optional<DetalleVenta> existente = detalles.stream()
+                .filter(det -> det.getProducto().getCodigo().equals(producto.getCodigo()))
+                .findFirst();
+
+        if (existente.isPresent()) {
+            // Actualizar cantidad y total del producto ya existente
+            DetalleVenta detExistente = existente.get();
+            detExistente.setCantidad(detExistente.getCantidad() + cantidad);
+            // Calcular el nuevo total
+            detExistente.setTotal(detExistente.getCantidad() * producto.getPrecioPublico());
+        } else {
+            // Crear un nuevo detalle
+            DetalleVenta detalleVenta = new DetalleVenta(cantidad, producto);
+            detalleVenta.setProducto(producto);
+            detalleVenta.setCantidad(cantidad);
+
+            detalles.add(detalleVenta);
+        }
+
+        // Calcular el subtotal del carrito
+        double sumaSubTotal = detalles.stream().mapToDouble(DetalleVenta::getTotal).sum();
+
+        // Calcular el IVA de la compra
+        double ivaComp = sumaSubTotal * 0.19;
+
+        // Calcular el IVA de la compra
+        double sumaTotal = sumaSubTotal + ivaComp;
+
+        // Pasar datos al modelo
+        model.addAttribute("carrito", detalles);
+        model.addAttribute("sumaSubTotal", sumaSubTotal); // SubTotal acumulado del carrito
+        model.addAttribute("ivaComp", ivaComp); // Iva de la compra del carrito
+        model.addAttribute("sumaTotal", sumaTotal); // Total compra
+        model.addAttribute("venta", venta);
+
+        return "user/carrito";
+    }
+
+    @GetMapping("/delete/carrito/{codigo}")
+    public String borrarProductoCarrito(@PathVariable Long codigo, Model model){
+
+        List<DetalleVenta> detallesNueva = new ArrayList<DetalleVenta>();
+
+        for (DetalleVenta detalleVenta : detalles) {
+            if (!detalleVenta.getProducto().getCodigo().equals(codigo)) {
+                detallesNueva.add(detalleVenta);
+            }
+        }
+
+        detalles = detallesNueva;
+
+        double sumaSubTotal = 0;
+        // Calcular el total del carrito
+        sumaSubTotal = detalles.stream().mapToDouble(DetalleVenta::getTotal).sum();
+
+        // Calcular el IVA de la compra
+        double ivaComp = sumaSubTotal * 0.19;
+
+        // Calcular el IVA de la compra
+        double sumaTotal = sumaSubTotal + ivaComp;
+
+        // Pasar datos al modelo
+        model.addAttribute("carrito", detalles);
+        model.addAttribute("sumaSubTotal", sumaSubTotal); // SubTotal del carrito
+        model.addAttribute("ivaComp", ivaComp); // Iva de la compra del carrito
+        model.addAttribute("sumaTotal", sumaTotal); // Total compra
+        model.addAttribute("venta", venta);
+
+        return "user/carrito";
+    }
+
+    @GetMapping("/carrito")
+    public String verCarrito(Model model) {
+
+        double sumaSubTotal = 0;
+        // Calcular el total del carrito aunque este vacio
+        sumaSubTotal = detalles.stream().mapToDouble(DetalleVenta::getTotal).sum();
+
+        // Calcular el IVA de la compra
+        double ivaComp = sumaSubTotal * 0.19;
+
+        // Calcular el IVA de la compra
+        double sumaTotal = sumaSubTotal + ivaComp;
+
+        // Pasar datos al modelo
+        model.addAttribute("carrito", detalles);
+        model.addAttribute("sumaSubTotal", sumaSubTotal); // SubTotal del carrito
+        model.addAttribute("ivaComp", ivaComp); // Iva de la compra del carrito
+        model.addAttribute("sumaTotal", sumaTotal); // Total compra
+        model.addAttribute("venta", venta);
+
+        return "user/carrito";
+    }
 }
